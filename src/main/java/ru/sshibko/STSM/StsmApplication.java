d6e939c3c10dbbd2e62a906f3e4d38e5d7ec20a3
Serge <sshibkojavadev@gmail.com>
2025-04-04 15:55:35 +0800

    init commit

---
+++ src/main/java/ru/sshibko/STSM/StsmApplication.java
@@ -0,0 +1,13 @@
+package ru.sshibko.STSM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StsmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StsmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/sshibko/STSM/StsmApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sshibko.STSM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StsmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
